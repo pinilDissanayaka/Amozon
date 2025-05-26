a82d7b4cf97c7f12b3bd14e99abcf5182129015a
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-05-26 12:00:34 +0530

    change str

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from amozon import search_amazon
+from utils import search_amazon
 
 
 products=search_amazon(base_url="https://www.amazon.com.au", postcode="2143", city_name="REGENTS PARK", search_keyword="foam cutter")
